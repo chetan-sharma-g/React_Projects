2d2ee6d145c0c226b9578ec5c65763d05900da25
Chetan Sharma <ch9412233@gmail.com>
2023-11-10 14:48:20 +0530

    New Project => Password Generator

---
+++ 03_PasswordGenerator/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-center text-3xl py-5 font-bold underline italic'>Password Generator</h1>
+    </div>
+  )
+}
+
+export default App
